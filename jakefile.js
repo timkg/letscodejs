bf7874fcbcfb61823e9d2722d3f8075edba0f173
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-02-16 12:34:02 +0000

    set up strict lint options

--- jakefile.js
+++ jakefile.js
@@ -1,17 +1,36 @@
-/*global desc, task, jake, fail, complete*/
-"use strict";
+(function() {
+	/*global desc, task, jake, fail, complete*/
+	"use strict";
 
-task('default', ['lint']);
+	task('default', ['lint']);
 
-desc('Lint everything');
-task('lint', [], function() {
-	var lint = require('./build/lint/lint_runner.js');
-	var files = new jake.FileList();
-	files.include('**/*.js');
+	desc('Lint everything');
+	task('lint', [], function() {
+		var lint = require('./build/lint/lint_runner.js');
+		var files = new jake.FileList();
+		files.include('**/*.js');
+		var options = nodeLintOptions();
+		lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+	});
 
-	var options = {
-		node: true
-	};
-
-	lint.validateFileList(files.toArray(), options, {});
-});
+	function nodeLintOptions() {
+		return {
+			bitwise:true,
+			curly:true,
+			eqeqeq:true,
+			forin:true,
+			immed:true,
+			latedef:true,
+			newcap:true,
+			noarg:true,
+			noempty:true,
+			nonew:true,
+			regexp:true,
+			undef:true,
+//			unused: true,
+			strict:true,
+			trailing:true,
+			node:true
+		};
+	}
+}());
