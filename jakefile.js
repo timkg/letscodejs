4ab4b406f0fa6f3d7eb6483b9755c698ca490dc8
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-02-19 23:53:32 +0000

    set up integration procedure

--- jakefile.js
+++ jakefile.js
@@ -2,6 +2,7 @@
 	/*global desc, task, jake, fail, complete*/
 	"use strict";
 
+	desc('Build and test');
 	task('default', ['lint']);
 
 	desc('Lint everything');
@@ -10,7 +11,23 @@
 		var files = new jake.FileList();
 		files.include('**/*.js');
 		var options = nodeLintOptions();
-		lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+		var passed = lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+		if (!passed) {
+			fail('lint failed');
+		}
+	});
+
+	desc('Integration');
+	task('integrate', ['default'], function() {
+		console.log("1. Make sure 'git status' is clean");
+		console.log("2. Build on the integration box");
+		console.log("	a. walk over to integration box");
+		console.log("	b. 'git pull'");
+		console.log("	c. 'jake'");
+		console.log("3. 'git checkout integration'");
+		console.log("4. 'git merge master --no-ff --log'");
+		console.log("5. 'git checkout master'");
+		
 	});
 
 	function nodeLintOptions() {
