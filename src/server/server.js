5219fe4d0563f55011ef893739290129bdf36753
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-02-23 17:02:52 +0000

    added basic server spike

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,16 @@
 "use strict";
 
-exports.number = function() {
-	return 3;
-};
+var server = require('http').createServer();
+
+server.on('request', function(request, response){
+	console.log('request received');
+
+	var body = 	'<!doctype html> <html> <head> <meta charset="utf-8">' +
+				' <title></title> </head> <body> hi</body> </html>';
+
+	response.end(body);
+});
+
+server.listen(8080);
+
+console.log('server started');
