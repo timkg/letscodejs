4efdce59db521d904a7d11bc6066fc705e5b9c70
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-02-23 17:14:46 +0000

    wrapped server spike in exports.start

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,18 @@
 "use strict";
 
-var server = require('http').createServer();
+exports.start = function() {
+	var server = require('http').createServer();
 
-server.on('request', function(request, response){
-	console.log('request received');
+	server.on('request', function(request, response){
+		console.log('request received');
 
-	var body = 	'<!doctype html> <html> <head> <meta charset="utf-8">' +
-				' <title></title> </head> <body> hi</body> </html>';
+		var body = 	'<!doctype html> <html> <head> <meta charset="utf-8">' +
+					' <title></title> </head> <body> hi</body> </html>';
 
-	response.end(body);
-});
+		response.end(body);
+	});
 
-server.listen(8080);
+	server.listen(8080);
 
-console.log('server started');
+	console.log('server started');
+};
