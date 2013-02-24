0d792b0ec3229f45eec80c10131446165b4f4fa5
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-02-24 21:23:45 +0000

    test for server response

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,21 +4,30 @@ var http, server;
 server = require("./server.js");
 http = require('http');
 
-exports.testServerRespondsToGetRequests = function(test) {
-	test.expect(1);
-	server.start();
-	http.get('http://localhost:8080', function(response) {
-		test.ok(response, 'GET http://localhost:8080 yielded a response');
-		test.done();
-	});
-};
-
 exports.tearDown = function(done) {
-	// we want our test to call server.stop().
-	// we need want to call done() in order for the test to work. If we don't call done(),
+	// we want our test to call server.stop() after all tests are finished.
+	// we need to call done() in order for the test to work. If we don't call done(),
 	// nodeunit can't verify that everything finished as expected.
 	// server.stop() is asynchronous, so we can't just call done() after calling server.stop(),
 	// because the proper order is not guaranteed. So we pass a reference to done as callback
 	// to server.stop(), which executes any callback parameters it encounters.
 	server.stop(done);
+};
+
+exports.test_serverReturnsHelloWorld = function(test) {
+	// test.expect(1);
+	server.start();
+	var request = http.get('http://localhost:8080');
+	request.on('response', function(response) {
+		response.setEncoding('utf8');
+		var responseData = '';
+		test.equals(response.statusCode, 200, 'response statusCode is 200');
+		response.on('data', function(chunk) {
+			responseData += chunk;
+		});
+		response.on('end', function() {
+			test.equals(responseData, 'Hello World', 'response equals Hello World');
+			test.done();
+		});
+	});
 };
--- src/server/server.js
+++ src/server/server.js
@@ -4,12 +4,8 @@ var server = require('http').createServer();
 
 exports.start = function() {
 	server.on('request', function(request, response){
-		console.log('request received');
-
-		var body =	'<!doctype html> <html> <head> <meta charset="utf-8">' +
-					' <title></title> </head> <body> hi</body> </html>';
-
-		response.end(body);
+		response.statusCode = 200;
+		response.end('Hello World');
 	});
 
 	server.listen(8080);
