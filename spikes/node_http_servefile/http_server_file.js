933664b73b8c048ef23fd5ad80831c9d12fe7796
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-03-12 22:29:18 +0000

    richer log message

--- spikes/node_http_servefile/http_server_file.js
+++ spikes/node_http_servefile/http_server_file.js
@@ -7,10 +7,10 @@ server.on('request', function(request, response){
 	console.log('request received');
 
 	fs.readFile('file.html', function(err, data) {
-		response.end(data);	
+		response.end(data);
 	});
 });
 
 server.listen(8080);
 
-console.log('server started');
+console.log('server started on port 8080');
