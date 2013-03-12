410ae33c91d7b10b9cfc37c65ab7080e55aeecb1
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-03-12 01:15:08 +0000

    created file spie

---
+++ spikes/node_http_servefile/http_server_file.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var server = require('http').createServer();
+var fs = require("fs");
+
+server.on('request', function(request, response){
+	console.log('request received');
+
+	fs.readFile('file.html', function(err, data) {
+		response.end(data);	
+	});
+});
+
+server.listen(8080);
+
+console.log('server started');
