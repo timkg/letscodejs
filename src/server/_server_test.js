37c743b8ff4cd28158d326c6ffab3eb0c8786f4f
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-03-13 00:26:27 +0000

    made server accept file to serve, updated tests

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,12 +1,28 @@
 "use strict";
 
-var http, server, fs;
+var http, server, fs, assert;
 server = require("./server.js");
 http = require('http');
 fs = require('fs');
+assert = require('assert');
 
-exports.test_serverReturnsHelloWorld = function(test) {
-	server.start(8080);
+var TEST_DIR = 'generated/test';
+var TEST_FILE = TEST_DIR + '/test.html';
+
+exports.tearDown = function(done) {
+	if( fs.existsSync(TEST_FILE) ) {
+		fs.unlinkSync(TEST_FILE);
+		assert.ok(!fs.existsSync(TEST_FILE), 'test file sould be deleted after test has run');
+	}
+	done();
+};
+
+exports.test_serverServesAFile = function(test) {
+	var testFileContent = "Hello World, what a coincidence";
+
+	fs.writeFileSync(TEST_FILE, testFileContent);
+
+	server.start(TEST_FILE, 8080);
 	var request = http.get('http://localhost:8080');
 	request.on('response', function(response) {
 		response.setEncoding('utf8');
@@ -16,30 +32,30 @@ exports.test_serverReturnsHelloWorld = function(test) {
 			responseData += chunk;
 		});
 		response.on('end', function() {
-			test.equals(responseData, 'Hello World', 'response equals Hello World');
-			server.stop(test.done);
+			test.equals(responseData, testFileContent, 'correct response received from file');
+			server.stop(function() {
+				test.done();
+			});
 		});
 	});
 };
 
-exports.test_serverServesAFile = function(test) {
-	var testDir = 'generated/test';
-	var testFile = testDir + '/test.html';
-
-	fs.writeFileSync(testFile, 'Hello World');
-
+exports.test_serverThrowsExceptionWhenNoHtmlFilePassed = function(test) {
+	test.throws(function() {
+		server.start( null, 8008 );
+	});
 	test.done();
 };
 
 exports.test_serverThrowsExceptionWhenNoPortnumberPassed = function(test) {
 	test.throws(function() {
-		server.start();
+		server.start(TEST_FILE);
 	});
 	test.done();
 };
 
 exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
-	server.start(8080);
+	server.start(TEST_FILE, 8080);
 	server.stop(test.done); // nodeunit would fail if test.done() wasn't called
 };
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,23 @@
 "use strict";
 
-var server = require('http').createServer();
+var server, fs;
+server = require('http').createServer();
+fs = require('fs');
 
-exports.start = function(portNumber) {
+
+exports.start = function(htmlFileToServe, portNumber) {
+	if( !htmlFileToServe ) {
+		throw new Error('server.start() requires html file');
+	}
+
 	if( !portNumber ) {
 		throw new Error('server.start() requires port number');
 	}
 
-	server.on('request', function(request, response){
-		
-		response.end('Hello World');
+	server.on('request', function(request, response) {
+		fs.readFile(htmlFileToServe, function(err, data) {
+			response.end(data);
+		});
 	});
 	server.listen(portNumber);
 };
