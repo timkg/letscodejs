5964b916ffe512d905ec7eeb8e21a0c1f494716d
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-03-17 17:53:18 +0000

    custom 404 page + tests

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -8,51 +8,84 @@
 	assert = require('assert');
 
 	var TEST_DIR = 'generated/test';
-	var TEST_FILE = TEST_DIR + '/test.html';
+	var TEST_HOMEPAGE = TEST_DIR + '/test.html';
+	var TEST_404_FILE = TEST_DIR + '/test404.html';
 
 	exports.tearDown = function(done) {
-		if( fs.existsSync(TEST_FILE) ) {
-			fs.unlinkSync(TEST_FILE);
-			assert.ok(!fs.existsSync(TEST_FILE), 'test file sould be deleted after test has run');
-		}
+		cleanUpFile(TEST_HOMEPAGE);
+		cleanUpFile(TEST_404_FILE);
 		done();
 	};
 
-	exports.test_serverServesHomepageFromFile = function(test) {
-		var testFileContent = "Hello World, what a coincidence";
-		fs.writeFileSync(TEST_FILE, testFileContent);
-		server.start(TEST_FILE, 8080);
+	exports.test_servesHomepageFromFile = function(test) {
+		var expectedData = "Hello World, what a coincidence";
+		fs.writeFileSync(TEST_HOMEPAGE, expectedData);
 
 		httpGet('http://localhost:8080', function(response, responseData) {
 			test.equals(response.statusCode, 200, 'response statusCode is 200');
-			test.equals(responseData, testFileContent, 'correct response received from file');
+			test.equals(responseData, expectedData, 'correct response received from file');
 			test.done();
 		});
 	};
 
-	exports.test_serverThrowsExceptionWhenNoHtmlFilePassed = function(test) {
+	exports.test_returnsHomepageWhenAskedForIndex = function(test) {
+		var expectedData = "Hello World, what a coincidence";
+		fs.writeFileSync(TEST_HOMEPAGE, expectedData);
+
+		httpGet('http://localhost:8080/index.html', function(response, responseData) {
+			test.equals(response.statusCode, 200, 'response statusCode is 200');
+			test.equals(responseData, expectedData, 'correct response received from file');
+			test.done();
+		});
+	};
+
+	exports.test_throwsExceptionWhenNoHtmlFilePassed = function(test) {
 		test.throws(function() {
-			server.start( null, 8008 );
+			server.start( null, TEST_404_FILE, 8008 );
 		});
 		test.done();
 	};
 
-	exports.test_serverThrowsExceptionWhenNoPortnumberPassed = function(test) {
+
+	exports.test_throwsExceptionWhenNo404FilePassed = function(test) {
 		test.throws(function() {
-			server.start(TEST_FILE);
+			server.start( TEST_HOMEPAGE, null, 8008 );
 		});
 		test.done();
 	};
 
-	exports.test_serverReturns404ForEverythingButHomepage = function(test) {
-		server.start(TEST_FILE, 8080);
+	exports.test_throwsExceptionWhenNoPortnumberPassed = function(test) {
+		test.throws(function() {
+			server.start( TEST_HOMEPAGE, TEST_404_FILE );
+		});
+		test.done();
+	};
+
+	exports.test_returns404FromFileForEverythingButHomepage = function(test) {
+		var expectedData = "404 not found";
+		fs.writeFileSync(TEST_404_FILE, expectedData);
+
 		httpGet('http://localhost:8080/garblegarble', function(response, responseData) {
 			test.equals(response.statusCode, 404, 'response statusCode is 404');
+			test.equals(responseData, expectedData, 'returns custom 404 file');
 			test.done();
 		});
+	};
+
+	exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
+		server.start(TEST_HOMEPAGE, TEST_404_FILE, 8080);
+		server.stop(test.done); // nodeunit would fail if test.done() wasn't called, that's why we pass the callback
+	};
+
+	exports.test_stopCalledTwiceInARowThrowsException = function(test) {
+		test.throws(function() {
+			server.stop();
+		});
+		test.done();
 	};
 
 	function httpGet(url, callback) {
+		server.start(TEST_HOMEPAGE, TEST_404_FILE, 8080);
 		var request = http.get(url);
 		request.on('response', function(response) {
 			var responseData = '';
@@ -70,16 +103,10 @@
 		});
 	}
 
-	exports.test_serverRunsCallbackWhenStopCompletes = function(test) {
-		server.start(TEST_FILE, 8080);
-		server.stop(test.done); // nodeunit would fail if test.done() wasn't called
-	};
-
-
-	exports.test_stopCalledTwiceInARowThrowsException = function(test) {
-		test.throws(function() {
-			server.stop();
-		});
-		test.done();
-	};
+	function cleanUpFile(fileName) {
+		if( fs.existsSync(fileName) ) {
+			fs.unlinkSync(fileName);
+			assert.ok(!fs.existsSync(fileName), 'test file sould be deleted after test has run');
+		}
+	}
 }());
--- src/server/server.js
+++ src/server/server.js
@@ -1,32 +1,43 @@
-"use strict";
+(function() {
+	"use strict";
 
-var server, fs;
-server = require('http').createServer();
-fs = require('fs');
+	var server, fs;
+	server = require('http').createServer();
+	fs = require('fs');
 
-
-exports.start = function(htmlFileToServe, portNumber) {
-	if( !htmlFileToServe ) {
-		throw new Error('server.start() requires html file');
-	}
-
-	if( !portNumber ) {
-		throw new Error('server.start() requires port number');
-	}
-
-	server.on('request', function(request, response) {
-		if( request.url === '/' ) {
-			fs.readFile(htmlFileToServe, function(err, data) {
-				response.end(data);
-			});
-		} else {
-			response.statusCode = 404;
-			response.end();
+	exports.start = function(htmlFileToServe, notFoundFileToServe, portNumber) {
+		if( !htmlFileToServe ) {
+			throw new Error('server.start() requires html file');
 		}
-	});
-	server.listen(portNumber);
-};
 
-exports.stop = function(callback) {
-	server.close(callback);
-};
+		if( !notFoundFileToServe ) {
+			throw new Error('server.start() requires 404 file');
+		}
+
+		if( !portNumber ) {
+			throw new Error('server.start() requires port number');
+		}
+
+		server.on('request', function(request, response) {
+			if( request.url === '/' || request.url === '/index.html') {
+				response.statusCode = 200;
+				serveFile(response, htmlFileToServe);
+			} else {
+				response.statusCode = 404;
+				serveFile(response, notFoundFileToServe);
+			}
+		});
+		server.listen(portNumber);
+	};
+
+	exports.stop = function(callback) {
+		server.close(callback);
+	};
+
+	function serveFile(response, file) {
+		fs.readFile(file, function(err, data) {
+			response.end(data);
+		});
+	}
+
+}());
