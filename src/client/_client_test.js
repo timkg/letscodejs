3c3047e108ecc3e61865542bf60ac9a2f95ac68f
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-03-29 16:14:44 +0000

    replaced chai with expect.js

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,9 +1,7 @@
-var assert = chai.assert;
-
 describe('Nothing', function() {
 
 	it('should run', function() {
-		assert.notEqual('foo', 'bar', 'should not be equal');
+		expect('foo').to.equal('foo');
 	});
 
 });
