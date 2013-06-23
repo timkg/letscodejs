5a3a5921540c8a64e813d915e7e9432d619d3fab
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-06-23 16:32:50 +0100

    testing touch events

--- src/client/_dom_element_test.js
+++ src/client/_dom_element_test.js
@@ -11,14 +11,37 @@
 		});
 
 		it('handles mouse events', function() {
-			domElement.on('mousemove', function(event, offset) {
-				console.log(offset);
+			['down', 'move', 'up', 'enter', 'leave', 'over'].map(function(type) {
+				testMouseEvent(type);
+			});
+		});
+
+		it('handles touch events', function() {
+			if (!supportsTouch()) { return; }
+			['start', 'move', 'end', 'cancel'].map(function(type) {
+				testTouchEvent(type);
+			});
+		});
+
+		function testMouseEvent(type) {
+			domElement.on('mouse'+type, function(event, offset) {
 				expect(offset.x).to.equal(10);
 				expect(offset.y).to.equal(15);
 			});
-			domElement.triggerMouse('move', 10, 15);
-		});
+			domElement.triggerMouse(type, 10, 15);
+		}
 
+		function testTouchEvent(type) {
+			domElement.onSingleTouch(type, function(event, offset) {
+				expect(offset.x).to.equal(10);
+				expect(offset.y).to.equal(15);
+			});
+			domElement.triggerTouch(type, 10, 15);
+		}
+
+		function supportsTouch() {
+			return (typeof Touch === 'object');
+		}
 
 	});
 
