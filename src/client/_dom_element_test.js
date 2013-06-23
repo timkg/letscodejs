f98ff015d48e1274b25e3ee8cbb87b8494735e77
Tim Koch-Grunberg <tim.theodor@gmail.com>
2013-06-23 16:08:51 +0100

    starting dom tests

--- src/client/_dom_element_test.js
+++ src/client/_dom_element_test.js
@@ -1,12 +1,24 @@
 (function () {
-	/*global describe, it, expect*/
+	/*global beforeEach, describe, it, expect, $, wwp*/
 	"use strict";
 
-	describe('nothing', function() {
+	describe('DOM Element', function() {
 
-		it('should fail', function() {
-			
+		var domElement;
+
+		beforeEach(function() {
+			domElement = new wwp.DomElement($('<div></div>'));
 		});
+
+		it('handles mouse events', function() {
+			domElement.on('mousemove', function(event, offset) {
+				console.log(offset);
+				expect(offset.x).to.equal(10);
+				expect(offset.y).to.equal(15);
+			});
+			domElement.triggerMouse('move', 10, 15);
+		});
+
 
 	});
 
